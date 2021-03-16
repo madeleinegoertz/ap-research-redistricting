2788fa1056f41028b4d875386567a2e47fc12489
madiemoxie <madiemoxie@outlook.com>
2021-03-15 17:52:14 -0700

    ran mcmc 10 sims on data.

---
+++ results/mcmc.r
@@ -0,0 +1,20 @@
+# This script redistricts using mcmc. Output stored in results/mcmc.RData
+# maps in mcmc.out$partitions.
+
+library(redist)
+
+load("results/data.RData")
+
+#use results from CRSG to generate starting values
+load("results/crsg.RData")
+
+mcmc.out <- redist.mcmc(adjobj = adjlist,
+                        popvec = df$pop,
+                        #ndists = 11,
+                        initcds = crsg.out$partitions[,1],
+                        nsims = 10,
+                        constraint = c("population", "compact"),
+                        constraintweights = c(1, 1),
+                        popcons = .01,
+                        ssdmat = distancemat)
+save(mcmc.out, file = "results/mcmc.RData")
