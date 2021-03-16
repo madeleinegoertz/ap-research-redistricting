c39871449b7a20536912933623759b4138f6a40f
madiemoxie <madiemoxie@outlook.com>
2021-03-16 12:35:08 -0700

    create long table to bring into python

--- results/table_compact.r
+++ results/table_compact.r
@@ -4,4 +4,15 @@ options(xtable.floating = TRUE)
 options(xtable.timestamp = "")
 
 load("results/calc.compact.RDATA")
-xtable
+
+library(tidyverse)
+longtbl <-
+  table %>%
+  pivot_longer(
+    !c(alg, nloop),
+    names_to = "measures",
+    values_to = "value"
+  )
+
+# R doesn't set me use multiindex so I have to send this to python.
+write.csv(longtbl, file = "results/longtbl.csv")
