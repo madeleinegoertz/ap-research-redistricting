3afd83c15b7795ce011834082e8d5cd89f7ec589
madiemoxie <madiemoxie@outlook.com>
2021-03-16 22:04:12 -0700

    wip making final table.

--- results/table_latex.py
+++ results/table_latex.py
@@ -1,10 +1,18 @@
+# This script creates the data table for my project
 import pandas as pd
 
-df = pd.read_csv("results/longtbl.csv", usecols=["nloop","alg","measures","value"])
-df = df.set_index(['alg', 'nloop', 'measures'])
-df = df.unstack(level='measures')
-df2 = df.mean(level=0)
-df2['nloop'] = "mean"
-df2 = df2.set_index('nloop', append=True)
-df = pd.concat([df, df2])
-print(df.to_latex())
+dfc = pd.read_csv("results/compact.csv", usecols=["nloop","alg","measure","value"])
+dff = pd.read_csv("results/fair.csv", usecols=["nloop","alg","measure","value"])
+
+df = pd.concat([dfc, dff])
+print(df)
+# dfc = dfc.rename(columns={"nloop": "map"})
+# dff = dff.rename(columns={"nloop": "map"})
+
+# df = df.set_index(['alg', 'nloop', 'measures'])
+# df = df.unstack(level='measures')
+# df2 = df.mean(level=0)
+# df2['nloop'] = "mean"
+# df2 = df2.set_index('nloop', append=True)
+# df = pd.concat([df, df2])
+# print(df.to_latex())
