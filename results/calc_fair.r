8be30c00a8dc0c9858101c07d4bd9d732c077269
madiemoxie <madiemoxie@outlook.com>
2021-03-16 22:02:07 -0700

    futs to create per map stats

---
+++ results/calc_fair.r
@@ -0,0 +1,160 @@
+library(tidyverse)
+
+load("results/raw.fair.RData")
+
+########################## MCMC
+# take mean DSeats
+table.mcmc <-
+  raw.fair$mcmc %>%
+  select(districts, DSeats, nloop) %>%
+  pivot_wider(
+    names_from = districts,
+    values_from = DSeats
+  ) %>%
+  rowwise(nloop) %>%
+  summarise(DSeats = mean(c_across()))
+# add partisan bias
+table.mcmc$bias <-
+  raw.fair$mcmc %>%
+  select(districts, Bias, nloop) %>%
+  pivot_wider(
+    names_from = districts,
+    values_from = Bias
+  ) %>%
+  rowwise(nloop) %>%
+  summarise(bias = mean(c_across())) %>%
+  pull(var = bias)
+# add dvs_mean
+table.mcmc$dvs_mean <-
+  raw.fair$mcmc %>%
+  select(districts, DVS, nloop) %>%
+  pivot_wider(
+    names_from = districts,
+    values_from = DVS
+  ) %>%
+  rowwise(nloop) %>%
+  summarise(dvs_mean = mean(c_across())) %>%
+  pull(var = dvs_mean)
+table.mcmc$alg <- "mcmc"
+
+########################## SMC
+# take mean DSeats
+table.smc <-
+  raw.fair$smc %>%
+  select(districts, DSeats, nloop) %>%
+  pivot_wider(
+    names_from = districts,
+    values_from = DSeats
+  ) %>%
+  rowwise(nloop) %>%
+  summarise(DSeats = mean(c_across()))
+# add partisan bias
+table.smc$bias <-
+  raw.fair$smc %>%
+  select(districts, Bias, nloop) %>%
+  pivot_wider(
+    names_from = districts,
+    values_from = Bias
+  ) %>%
+  rowwise(nloop) %>%
+  summarise(bias = mean(c_across())) %>%
+  pull(var = bias)
+# add dvs_mean
+table.smc$dvs_mean <-
+  raw.fair$smc %>%
+  select(districts, DVS, nloop) %>%
+  pivot_wider(
+    names_from = districts,
+    values_from = DVS
+  ) %>%
+  rowwise(nloop) %>%
+  summarise(dvs_mean = mean(c_across())) %>%
+  pull(var = dvs_mean)
+table.smc$alg <- "smc"
+
+########################## CRSG
+# take mean DSeats
+table.crsg <-
+  raw.fair$crsg %>%
+  select(districts, DSeats, nloop) %>%
+  pivot_wider(
+    names_from = districts,
+    values_from = DSeats
+  ) %>%
+  rowwise(nloop) %>%
+  summarise(DSeats = mean(c_across()))
+# add partisan bias
+table.crsg$bias <-
+  raw.fair$crsg %>%
+  select(districts, Bias, nloop) %>%
+  pivot_wider(
+    names_from = districts,
+    values_from = Bias
+  ) %>%
+  rowwise(nloop) %>%
+  summarise(bias = mean(c_across())) %>%
+  pull(var = bias)
+# add dvs_mean
+table.crsg$dvs_mean <-
+  raw.fair$crsg %>%
+  select(districts, DVS, nloop) %>%
+  pivot_wider(
+    names_from = districts,
+    values_from = DVS
+  ) %>%
+  rowwise(nloop) %>%
+  summarise(dvs_mean = mean(c_across())) %>%
+  pull(var = dvs_mean)
+table.crsg$alg <- "crsg"
+
+########################## Control
+# take mean DSeats
+table.control <-
+  raw.fair$control %>%
+  select(districts, DSeats, nloop) %>%
+  pivot_wider(
+    names_from = districts,
+    values_from = DSeats
+  ) %>%
+  rowwise(nloop) %>%
+  summarise(DSeats = mean(c_across()))
+# add partisan bias
+table.control$bias <-
+  raw.fair$control %>%
+  select(districts, Bias, nloop) %>%
+  pivot_wider(
+    names_from = districts,
+    values_from = Bias
+  ) %>%
+  rowwise(nloop) %>%
+  summarise(bias = mean(c_across())) %>%
+  pull(var = bias)
+# add dvs_mean
+table.control$dvs_mean <-
+  raw.fair$control %>%
+  select(districts, DVS, nloop) %>%
+  pivot_wider(
+    names_from = districts,
+    values_from = DVS
+  ) %>%
+  rowwise(nloop) %>%
+  summarise(dvs_mean = mean(c_across())) %>%
+  pull(var = dvs_mean)
+table.control$alg <- "control"
+
+table <-
+  bind_rows(
+    table.mcmc, table.smc, table.crsg, table.control
+  )
+
+save(table, file = "results/calc.fair.RData")
+
+longtbl <-
+  table %>%
+  pivot_longer(
+    !c(alg, nloop),
+    names_to = "measure",
+    values_to = "value"
+  )
+# R doesn't set me use multiindex so I have to send this to python.
+write.csv(longtbl, file = "results/fair.csv")
