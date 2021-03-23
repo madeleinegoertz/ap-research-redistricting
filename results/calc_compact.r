b99547bcbfa75c2a212b1557b7e7380ca74c0c9a
madiemoxie <madiemoxie@outlook.com>
2021-03-23 13:27:38 -0700

    rewrite calc compact to be efficient

--- results/calc_compact.r
+++ results/calc_compact.r
@@ -3,119 +3,33 @@ load("results/data.RData")
 load("results/raw.compact.100.three.RData")
 
 ########################## MCMC
-# take mean pp score
-table.mcmc <-
-  raw.compact$mcmc %>%
-  select(districts, PolsbyPopper, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = PolsbyPopper
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(pp_mean = mean(c_across()))
-# add fh score
-table.mcmc$fh <-
-  raw.compact$mcmc %>%
-  select(districts, FryerHolden, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = FryerHolden
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(fh = mean(c_across())) %>%
-  pull(var = fh)
-# add Edges Removed score
-table.mcmc$ecc <-
-  raw.compact$mcmc %>%
-  select(districts, EdgesRemoved, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = EdgesRemoved
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(ecc = 1 - mean(c_across())/num_edges) %>%
-  pull(var = ecc)
-table.mcmc$alg <- "mcmc"
+calc_compact <- function(data, alg) {
+  df <-
+    data %>%
+    select(districts, PolsbyPopper, FryerHolden, EdgesRemoved, nloop) %>%
+    pivot_wider(
+      names_from = districts,
+      values_from = c(PolsbyPopper, FryerHolden, EdgesRemoved)
+    ) %>%
+    rowwise(nloop) %>%
+    summarise(
+      pp = mean(c_across(starts_with("PolsbyPopper"))),
+      fh = mean(c_across(starts_with("FryerHolden"))),
+      # normalize to total num edges. 
+      ecc = 1 - mean(c_across(starts_with("EdgesRemoved")))/num_edges
+    )
+  df$alg <- alg
+  return(df)
+}
 
-########################## SMC
-# take mean pp score
-table.smc <-
-  raw.compact$smc %>%
-  select(districts, PolsbyPopper, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = PolsbyPopper
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(pp_mean = mean(c_across()))
-# add fh score
-table.smc$fh <-
-  raw.compact$smc %>%
-  select(districts, FryerHolden, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = FryerHolden
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(fh = mean(c_across())) %>%
-  pull(var = fh)
-# add Edges Removed score
-table.smc$ecc <-
-  raw.compact$smc %>%
-  select(districts, EdgesRemoved, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = EdgesRemoved
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(ecc = 1 - mean(c_across())/num_edges) %>%
-  pull(var = ecc)
-table.smc$alg <- "smc"
-
-########################## Control
-# take mean pp score
-table.control <-
-  raw.compact$control %>%
-  select(districts, PolsbyPopper, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = PolsbyPopper
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(pp_mean = mean(c_across()))
-# add fh score
-table.control$fh <-
-  raw.compact$control %>%
-  select(districts, FryerHolden, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = FryerHolden
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(fh = mean(c_across())) %>%
-  pull(var = fh)
-# add Edges Removed score
-table.control$ecc <-
-  raw.compact$control %>%
-  select(districts, EdgesRemoved, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = EdgesRemoved
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(ecc = 1 - mean(c_across())/num_edges) %>%
-  pull(var = ecc)
-table.control$alg <- "control"
-
-table <-
-  bind_rows(
-    table.mcmc, table.smc, table.control
-  )
-
-save(table, file = "results/calc.compact.100.RData")
+table.mcmc <- calc_compact(raw.compact$mcmc, "mcmc")
+table.smc <- calc_compact(raw.compact$smc, "smc")
+table.control <- calc_compact(raw.compact$control, "control")
 
 longtbl <-
-  table %>%
+  bind_rows(
+    table.mcmc, table.smc, table.control
+  ) %>%
   pivot_longer(
     !c(alg, nloop),
     names_to = "measure",
