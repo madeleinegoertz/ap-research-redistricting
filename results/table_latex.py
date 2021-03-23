fa76749e51cd87213d6b5d8210ee082c5bf6438b
madiemoxie <madiemoxie@outlook.com>
2021-03-23 14:20:59 -0700

    created ok ish table

--- results/table_latex.py
+++ results/table_latex.py
@@ -10,4 +10,14 @@ df = pd.concat([dfc, dff])
 df = df.set_index(['alg', 'nloop', 'measure'])
 df = df.unstack(level='measure')
 mean = df.mean(level=0)
-mean.to_latex(buf="paper/results/tbl.tex")
+mean.columns = mean.columns.droplevel(0)
+mean_long = mean.transpose()
+# df2['nloop'] = "mean"
+# df2 = df2.set_index('nloop', append=True)
+mean_long = mean_long.reindex(["ecc", "fh", "pp", "dseats", "bias",
+   "dec", "effgap", "effgapeqpop",  "lopwin", "meanmed", "resp", "taugap"])
+mean_long["type"] = ["compact", "compact", "compact", "fair", "fair", 
+    "fair", "fair", "fair", "fair", "fair", "fair", "fair"]
+mean_long = mean_long.reset_index()
+mean_long = mean_long.set_index(['type', 'measure'])
+mean_long.to_latex(buf="paper/results/tbl.tex", float_format="{:#.6g}".format, multirow=True)
