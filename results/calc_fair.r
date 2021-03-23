bdbdd9b28b6716ad01d1b48a08db21f9da5d47b8
madiemoxie <madiemoxie@outlook.com>
2021-03-23 13:21:59 -0700

    rewrite calc fair to be efficient/include all measures.

--- results/calc_fair.r
+++ results/calc_fair.r
@@ -2,120 +2,38 @@ library(dplyr)
 
 load("results/raw.fair.100.all.RData")
 
-########################## MCMC
-# take mean DSeats
-table.mcmc <-
-  raw.fair$mcmc %>%
-  select(districts, DSeats, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = DSeats
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(DSeats = mean(c_across()))
-# add partisan bias
-table.mcmc$bias <-
-  raw.fair$mcmc %>%
-  select(districts, Bias, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = Bias
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(bias = mean(c_across())) %>%
-  pull(var = bias)
-# add dvs_mean
-table.mcmc$dvs_mean <-
-  raw.fair$mcmc %>%
-  select(districts, DVS, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = DVS
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(dvs_mean = mean(c_across())) %>%
-  pull(var = dvs_mean)
-table.mcmc$alg <- "mcmc"
+calc_fair <- function(data, alg) {
+  df <-
+    data %>%
+    select(districts, DSeats, Bias, EffGap, EffGapEqPop, TauGap, MeanMedian,
+           Declination, Responsiveness, LopsidedWins, nloop) %>%
+    pivot_wider(
+      names_from = districts,
+      values_from = c(DSeats, Bias, EffGap, EffGapEqPop, TauGap, MeanMedian,
+                      Declination, Responsiveness, LopsidedWins)
+    ) %>%
+    rowwise(nloop) %>%
+    summarise( # take mean of all measures across districts. 
+      dseats = mean(c_across(starts_with("DSeats"))),
+      bias = mean(c_across(starts_with("Bias"))),
+      effgap = mean(c_across(starts_with("EffGap_"))), # _ to not include == pop
+      effgapeqpop = mean(c_across(starts_with("EffGapEqPop"))),
+      taugap = mean(c_across(starts_with("TauGap"))),
+      meanmed = mean(c_across(starts_with("MeanMedian"))),
+      dec = mean(c_across(starts_with("Declination"))),
+      resp = mean(c_across(starts_with("Responsiveness"))),
+      lopwin = mean(c_across(starts_with("LopsidedWins"))),
+    ) 
+  df$alg <- alg
+  return(df)
+}
 
-########################## SMC
-# take mean DSeats
-table.smc <-
-  raw.fair$smc %>%
-  select(districts, DSeats, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = DSeats
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(DSeats = mean(c_across()))
-# add partisan bias
-table.smc$bias <-
-  raw.fair$smc %>%
-  select(districts, Bias, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = Bias
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(bias = mean(c_across())) %>%
-  pull(var = bias)
-# add dvs_mean
-table.smc$dvs_mean <-
-  raw.fair$smc %>%
-  select(districts, DVS, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = DVS
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(dvs_mean = mean(c_across())) %>%
-  pull(var = dvs_mean)
-table.smc$alg <- "smc"
-
-########################## Control
-# take mean DSeats
-table.control <-
-  raw.fair$control %>%
-  select(districts, DSeats, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = DSeats
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(DSeats = mean(c_across()))
-# add partisan bias
-table.control$bias <-
-  raw.fair$control %>%
-  select(districts, Bias, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = Bias
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(bias = mean(c_across())) %>%
-  pull(var = bias)
-# add dvs_mean
-table.control$dvs_mean <-
-  raw.fair$control %>%
-  select(districts, DVS, nloop) %>%
-  pivot_wider(
-    names_from = districts,
-    values_from = DVS
-  ) %>%
-  rowwise(nloop) %>%
-  summarise(dvs_mean = mean(c_across())) %>%
-  pull(var = dvs_mean)
-table.control$alg <- "control"
-
-table <-
-  bind_rows(
-    table.mcmc, table.smc, table.control
-  )
-
-save(table, file = "results/calc.fair.100.RData")
+table.mcmc <- calc_fair(raw.fair$mcmc, "mcmc")
+table.smc <- calc_fair(raw.fair$smc, "smc")
+table.control <- calc_fair(raw.fair$control, "control")
 
 longtbl <-
-  table %>%
+  bind_rows(table.mcmc, table.smc, table.control) %>%
   pivot_longer(
     !c(alg, nloop),
     names_to = "measure",
