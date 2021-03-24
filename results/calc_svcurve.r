04f6230dc96628e5f1fd7dc673b6859aede91149
madiemoxie <madiemoxie@outlook.com>
2021-03-23 23:13:01 -0700

    Wip seats votes curve with multiple lines seems to work ok.

--- results/calc_svcurve.r
+++ results/calc_svcurve.r
@@ -6,11 +6,36 @@ load("results/raw.fair.100.all.RData")
 
 # First need to extract DVS.
 
-dvs <-
-  raw.fair$mcmc %>%
+dvs <- function(data) {
+  data %>%
   select(districts, DVS, nloop) %>%
   pivot_wider(
-    names_from = districts,
+    names_from = nloop,
     values_from = DVS
   )
+}
+
+summarize_dvs <- function(data) {
+  data %>%
+  rowwise(districts) %>%
+  summarise(
+    mean = mean(c_across()),
+    median = median(c_across())
+  )
+}
+
+plot_dvs <- function(dvs, alg) {
+  dvs %>%
+  seatsVotes(desc=alg) %>%
+  plot(type="seatsVotes")
+}
+
+mcmc.raw <- dvs(raw.fair$mcmc)
+mcmc.sum <- summarize_dvs(mcmc.raw)
+plot_dvs(mcmc.sum$mean, 'MCMC')
+
+smc.raw <- dvs(raw.fair$smc)
+smc.sum <- summarize_dvs(smc.raw)
+plot_dvs(smc.raw[,100], 'SMC')
+
 
