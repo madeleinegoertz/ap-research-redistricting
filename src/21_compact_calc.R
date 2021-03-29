29f11d9803115bdee445eea6cf88445970a1b042
madiemoxie <madiemoxie@outlook.com>
2021-03-29 14:25:02 -0700

    calc compact measures

--- src/21_compact_calc.R
+++ src/21_compact_calc.R
@@ -1,20 +1,20 @@
 library(dplyr)
-load("data/data.RData")
-load("data/compact.raw.RData")
+load("data/compact.raw.100.RData")
+
+num_edges <-6991
 
 ########################## MCMC
 calc_compact <- function(data, alg) {
   df <-
     data %>%
-    select(districts, PolsbyPopper, FryerHolden, EdgesRemoved, nloop) %>%
+    select(districts, PolsbyPopper, EdgesRemoved, nloop) %>%
     pivot_wider(
       names_from = districts,
-      values_from = c(PolsbyPopper, FryerHolden, EdgesRemoved)
+      values_from = c(PolsbyPopper, EdgesRemoved)
     ) %>%
     rowwise(nloop) %>%
     summarise(
       pp = mean(c_across(starts_with("PolsbyPopper"))),
-      fh = mean(c_across(starts_with("FryerHolden"))),
       # normalize to total num edges. 
       ecc = 1 - mean(c_across(starts_with("EdgesRemoved")))/num_edges
     )
@@ -22,20 +22,19 @@ calc_compact <- function(data, alg) {
   return(df)
 }
 
-table.mcmc <- calc_compact(raw.compact$mcmc, "mcmc")
-table.mcmc <- table.mcmc[-nrow(table.mcmc),] # remove junk row
 table.smc <- calc_compact(raw.compact$smc, "smc")
+table.crsg <- calc_compact(raw.compact$crsg, "crsg")
 table.control <- calc_compact(raw.compact$control, "control")
-calc.compact <- list (
-  mcmc = table.mcmc, 
+calc.compact <- list ( 
   smc = table.smc,
+  crsg = table.crsg,
   control = table.control
 )
-save(calc.compact, file = "data/compact.calc.RData")
+save(calc.compact, file = "data/compact.calc.100.RData")
 
 longtbl <-
   bind_rows(
-    table.mcmc, table.smc, table.control
+    table.smc, table.crsg, table.control
   ) %>%
   pivot_longer(
     !c(alg, nloop),
@@ -43,5 +42,5 @@ longtbl <-
     values_to = "value"
   )
 # R doesn't set me use multiindex so I have to send this to python.
-write.csv(longtbl, file = "data/compact.csv")
+write.csv(longtbl, file = "data/compact.100.csv")
 
