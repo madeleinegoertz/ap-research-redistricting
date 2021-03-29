6b23ebff982d3b267f246a36ac3850862323ff90
madiemoxie <madiemoxie@outlook.com>
2021-03-28 23:12:28 -0700

    set up redist_maps obj

--- src/01_data_prep.R
+++ src/01_data_prep.R
@@ -15,24 +15,21 @@ library(spData)
 library(spdep)
 
 # unzip zip file that has the shapefiles in into temp directory
-unzip("src/va_ushouse_2018_precincts_data.zip", exdir="src/va_ushouse_2018_precincts_data")
+unzip("shp/va_ushouse_2018_precincts_race.zip", exdir="shp/va_ushouse_2018_precincts_race")
 # read in the shapefile
-df <- st_read("src/va_ushouse_2018_precincts_data/va_ushouse_2018_precincts_data.shp")
+df <- st_read("shp/va_ushouse_2018_precincts_race/va_ushouse_2018_precincts_race.shp")
 # delete the extracted files. 
-unlink("src/va_ushouse_2018_precincts_data", recursive=TRUE)
+unlink("shp/va_ushouse_2018_precincts_race", recursive=TRUE)
 
 # convert CON_DIST to integer for initcds
 df$CON_DIST <- as.numeric(df$CON_DIST)
 
-adjlist <- redist.adjacency(df)
-centroids <- sf::st_centroid(df)
-distancemat <- sf::st_distance(centroids, centroids)
-area <- sf::st_area(df)
-centers <- sf::st_coordinates(centroids)
 # add row_n as unique precinct identifier to join on for plots
 df$row_n <- seq.int(nrow(df))
-# CRSG requires integer population
-df$pop_int <- round(df$pop)
+
+# set up the redistricting problem
+va_map <- redist_map(df, existing_plan=CON_DIST, pop_tol=0.01)
+
 colors <- c("#DA618D", "#85D7D4", "#044b62", "#D5D160", "#e7854b", "#6cc32f", "#A87ADA", "#DAB8D3", "#C843DB", "#329349", "#78A5D5")
 
-save.image(file = "data/data.RData")
+save(va_map, colors, file = "data/data_race.RData")
